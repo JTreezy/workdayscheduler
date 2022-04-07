307ee2a2ba8e6586dfa5ea7323b04c86282e4412
Jonathan Tran <digitalsigna@gmail.com>
2022-04-07 15:54:51 -0700

    full functionality with local storage saving capabilities, added css properties to mimic mockup appearance

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
-var timeDisplayEl = $('#time-display')
+let timeDisplayEl = $('#time-display')
+let saveBtn = document.querySelector("#saveBtn")
+let description = document.querySelector("#event")
+let rows = $('#schedule').children(".row")
 const HOUR_OFFSET = 9
 
 displayTime()
@@ -24,11 +27,35 @@ function setTime(rows, i) {
 function displayTime() {
     let currentTime = moment().format('MMM DD, YYYY [at] hh:mm:ss a')
     timeDisplayEl.text(currentTime)
-    let rows = $('#schedule').children(".row")
     rows.each(function(i) {
         setTime(rows, i)
         setEvent(rows, i)
     })
 }
-setInterval(displayTime,100000000)
+setInterval(displayTime,1000)
 
+rows.each(function(i) {
+    let eventCell = rows.eq(i).find(".event")
+    let saveBtn = rows.eq(i).find(".saveBtn")
+    renderDescription(i)
+
+    saveBtn.on("click", function(event) {
+        event.preventDefault();
+
+        let description = eventCell.text()
+
+        if (description !== "") {
+            localStorage.setItem(`description${i}`, description)
+            renderDescription(i);
+        }
+    })
+})
+
+function renderDescription(i) {
+    let description = localStorage.getItem(`description${i}`)
+    if (!description) {
+        description = ""
+    }
+    let eventCell = rows.eq(i).find(".event")
+    eventCell.text(description)
+}
